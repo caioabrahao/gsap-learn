52520823e359c342e0ad5ba4990ddd3ac9dca929
Caio Abrahão <contato.caioabrahao@gmail.com>
2024-12-04 16:25:47 -0300

    added timeline feature

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { gsap } from 'gsap';
 
-gsap.to('.box', { x: 300,});
-gsap.to('.box', { y: -300, delay: .5});
-gsap.to('.box', { x: -300, delay: 1});
-gsap.to('.box', { y: 0, delay: 1.5,});
-gsap.to('.box', { x: 0, delay: 2,});
-gsap.to('.box', { height: 50, width: 250, delay: 2.5, onComplete: () => { spawnButton(); },});
+let tl = gsap.timeline();
+
+tl.to('.box', { x: 300,});
+tl.to('.box', { y: -300,},);
+tl.to('.box', { x: -300,});
+tl.to('.box', { y: 0,});
+tl.to('.box', { x: 0,});
+tl.to('.box', { height: 50, width: 250, onComplete: () => { spawnButton(); },});
 
 function spawnButton(){
     gsap.set('.box', {display: 'none',});
@@ -22,9 +24,15 @@ window.summonBlocks = summonBlocks;
 gsap.from(".hero h1", { duration: 1, x: 300, opacity: 0, scale: 0.5, rotation: 360, ease: "elastic",});
 gsap.from(".hero p", { duration: 1, opacity: 0, y: 100, ease: "bounce", delay: .5,});
 
-gsap.to(".grid-element", { scale: .8, duration: 5, ease: "back.out", stagger:{
-    each: 0.1,
+let tl2 = gsap.timeline({repeat: -1, repeatDelay: 1,});
+
+tl2.to(".grid-element", { scale: .8, duration: 2, ease: "back.out", stagger:{
+    each: 0.2,
     from: "center",
     grid: "auto",
-    repeat: 1,
+}});
+tl2.to(".grid-element", { scale: 1, duration: 2, ease: "back.out", stagger:{
+    each: 0.3,
+    from: "center",
+    grid: "auto",
 }});
